aa4d6adbbbf3de76547bb2c41c1450bedbac8758
MarcusConduru <marcus.conduru@gmail.com>
2022-02-23 18:37:33 -0300

    refactor: change httpPostClient to receive an object

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -7,6 +7,8 @@ export class RemoteAuthenticaion {
   ) {}
 
   async auth (): Promise<void> {
-    await this.httpPostClient.post(this.url)
+    await this.httpPostClient.post({
+      url: this.url
+    })
   }
 }
