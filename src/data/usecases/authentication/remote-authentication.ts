5779c7a99621aedd16eaa684ab5b221c84df9c9e
MarcusConduru <marcus.conduru@gmail.com>
2022-02-24 19:25:21 -0300

    feat: ensure remoteAuthentication retuns an Account on sucess

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,22 +3,22 @@ import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-erros";
 import { UnexpectError } from "@/domain/errors/unexpect-erros";
 import { AccountModel } from "@/domain/models/account-model";
-import { AuthenticaionParams } from "@/domain/usecases/authenticaion";
+import { AuthenticaionParams, Authentication } from "@/domain/usecases/authenticaion";
 
-export class RemoteAuthenticaion {
+export class RemoteAuthenticaion implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticaionParams, AccountModel>
   ) {}
 
-  async auth (params: AuthenticaionParams): Promise<void> {
+  async auth (params: AuthenticaionParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
 
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: break
+      case HttpStatusCode.ok: return httpResponse.body
       case HttpStatusCode.unathorized: throw new InvalidCredentialsError()
       default: throw new UnexpectError()
     }
--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,7 +1,12 @@
 import { AuthenticaionParams } from "@/domain/usecases/authenticaion";
 import faker from "faker";
+import { AccountModel } from "../models/account-model";
 
 export const mockAuthentication = (): AuthenticaionParams => ({
   email: faker.internet.email(),
   password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.datatype.uuid()
 })
