4639e58f0be09602025863d6c5ac3940f4d76921
MarcusConduru <marcus.conduru@gmail.com>
2022-03-03 16:17:13 -0300

    feat: add app router

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import { Login } from '@/presentation/pages'
+import Login from '@/presentation/pages/login/login'
+import '@/presentation/styles/global.scss'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,10 @@
+import Footer from '@/presentation/components/footer/footer'
+import FormStatus from '@/presentation/components/form-status/form-status'
+import Input from '@/presentation/components/input/input'
+import LoginHeader from '@/presentation/components/login-header/login-header'
 import React from 'React'
 import Styles from './login-styles.scss'
-import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
+// import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
 
 const login: React.FC = () => {
   return (
