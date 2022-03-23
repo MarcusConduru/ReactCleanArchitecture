51aee112861c0008776a88aa933e788c7bf423c6
marcus.conduru <marcus.conduru@gmail.com>
2022-03-23 18:03:57 -0300

    test: ensure SignUp calls addACcount with correct values

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -7,11 +7,12 @@ import {
   waitFor,
 } from '@testing-library/react';
 import SignUp from './signup';
-import { Helper, ValidationStub } from '@/presentation/test';
+import { AddAccountSpy, Helper, ValidationStub } from '@/presentation/test';
 import faker from 'faker';
 
 type SutTypes = {
   sut: RenderResult;
+  addAccountSpy: AddAccountSpy;
 };
 
 type SutParams = {
@@ -20,11 +21,15 @@ type SutParams = {
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub();
+  const addAccountSpy = new AddAccountSpy();
   validationStub.errorMessage = params?.validationError;
-  const sut = render(<SignUp validation={validationStub} />);
+  const sut = render(
+    <SignUp validation={validationStub} addAccount={addAccountSpy} />,
+  );
 
   return {
     sut,
+    addAccountSpy,
   };
 };
 
@@ -122,5 +127,19 @@ describe('SignUp Component', () => {
     const { sut } = makeSut();
     await simulateValidSubmit(sut);
     Helper.testElementExists(sut, 'spinner');
+  });
+
+  test('Should call AddAccount with correct values', async () => {
+    const { sut, addAccountSpy } = makeSut();
+    const name = faker.name.findName();
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+    await simulateValidSubmit(sut, name, email, password);
+    expect(addAccountSpy.params).toEqual({
+      name,
+      email,
+      password,
+      passwordConfirmation: password,
+    });
   });
 });
