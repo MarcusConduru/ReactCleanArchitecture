402e0719dc6a7c2ef05c41e312145e3b653f0e80
marcus.conduru <marcus.conduru@gmail.com>
2022-03-30 17:41:15 -0300

    fix: jest

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
     '!<rootDir>/src/domain/models/index.ts',
     '!<rootDir>/src/domain/usecases/index.ts',
     '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/presentation/pages/index.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
