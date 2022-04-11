da842305d1384801da7425b60cbf0517ecb06518
marcus.conduru <marcus.conduru@gmail.com>
2022-04-11 15:18:31 -0300

    feat: prevent login to call submit multiple times

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -63,13 +63,30 @@ describe('Login', () => {
     cy.url().should('eq', `${baseUrl}/login`)
   });
 
-  it('Should present UnexpectedError on 400', () => {
+  it('Should present UnexpectedError on default error cases', () => {
     cy.route({
       method: 'POST',
       url: /login/,
       status: 400,
       response: {
         error: faker.random.word()
+      }
+    })
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').click();
+    cy.getByTestId('spinner').should('not.exist')
+    cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve');
+    cy.url().should('eq', `${baseUrl}/login`)
+  });
+
+  it('Should present UnexpectedError if invalid data is returned', () => {
+    cy.route({
+      method: 'POST',
+      url: /login/,
+      status: 200,
+      response: {
+        invalidProperty: faker.datatype.uuid()
       }
     })
     cy.getByTestId('email').focus().type(faker.internet.email())
@@ -98,20 +115,18 @@ describe('Login', () => {
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   });
 
-  it('Should present UnexpectedError if invalid data is returned', () => {
+  it('Should prevent multiple submits', () => {
     cy.route({
       method: 'POST',
       url: /login/,
       status: 200,
       response: {
-        invalidProperty: faker.datatype.uuid()
+        accessToken: faker.datatype.uuid()
       }
-    })
+    }).as('request')
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
-    cy.getByTestId('submit').click();
-    cy.getByTestId('spinner').should('not.exist')
-    cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve');
-    cy.url().should('eq', `${baseUrl}/login`)
+    cy.getByTestId('submit').dblclick();
+    cy.get('@request.all').should('have.length', 1)
   });
 });
