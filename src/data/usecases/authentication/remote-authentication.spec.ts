ac283abfb353091c3455ad327bc03799f9270b48
marcus.conduru <marcus.conduru@gmail.com>
2022-04-15 14:52:55 -0300

    test: ensure RemoteLoadSurveyList returns a list on to 200

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -78,7 +78,7 @@ describe('RemoteAuthentication', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 
-  test('Should return an AccountModel if HttpPostClient returns returns 200', async () => {
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
     const { sut, httpPostClientSpy } = makeSut();
     const httpResult = mockAccountModel();
     httpPostClientSpy.response = {
