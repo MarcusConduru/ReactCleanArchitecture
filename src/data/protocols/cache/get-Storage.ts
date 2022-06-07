fa2e2b03cf76e661dff803c4704529c49483c5d0
marcus.conduru <marcus.conduru@gmail.com>
2022-06-07 15:16:27 -0300

    feat: ensure LocalStorageAdapter calls localStorage.getItem with correct value

---
+++ src/data/protocols/cache/get-Storage.ts
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-types */
+export interface GetStorage {
+  get(key: string): any;
+}
---
+++ src/data/protocols/cache/index.ts
@@ -0,0 +1,2 @@
+export * from './get-Storage';
+export * from './set-storage';
