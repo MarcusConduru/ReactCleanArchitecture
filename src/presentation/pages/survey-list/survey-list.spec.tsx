908b1425c1f3a1140ae997958be755d55447845e
MarcusConduru <marcus.conduru@gmail.com>
2022-07-11 16:40:43 -0300

    test: ensure SurveyList components calls LoadSurveyList

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,15 +1,39 @@
-import SurveyList from './survey-list'
+import SurveyList from './survey-list';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { LoadSurveyList } from '@/domain/usecases';
+import { SurveyModel } from '@/domain/models';
 
-const makeSut = (): void => {
-  render(<SurveyList />);
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount = 0;
+  async loadAll(): Promise<SurveyModel[]> {
+    this.callsCount++;
+    return [];
+  }
 }
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy();
+  render(<SurveyList loadSurveyList={loadSurveyListSpy} />);
+
+  return {
+    loadSurveyListSpy,
+  };
+};
 
 describe('SurveyList Component', () => {
   test('Should present 4 empty items on start', () => {
-    makeSut()
-    const surveyList = screen.getByTestId('survey-list')
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+    makeSut();
+    const surveyList = screen.getByTestId('survey-list');
+    expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
+  });
+
+  test('Should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut();
+    expect(loadSurveyListSpy.callsCount).toBe(1);
   });
 });
