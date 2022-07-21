c8308fbfe6040f9637e604b7f0995fb6afe419d2
MarcusConduru <marcus.conduru@gmail.com>
2022-07-21 15:57:52 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,12 +1,8 @@
 /* eslint-disable prettier/prettier */
-import { UnexpectedError } from '@/domain/errors';
 import { AccountModel } from '@/domain/models';
 import { makeLocalLocalStorage } from '../factories/cache/local-storage-adapeter-factory';
 
 export const setCurrentAccountAdpter = (account: AccountModel): void => {
-  if (!account?.accessToken) {
-    throw new UnexpectedError();
-  }
   makeLocalLocalStorage().set('account', account);
 };
 
