9497c747c1d2a8a9ceb0b2390ef79ef55a0d1ef3
MarcusConduru <marcus.conduru@gmail.com>
2022-09-27 20:27:44 -0300

    test: ensure SurveyList logout if no token is found

---
+++ src/main/test/cypress/integration/private-routes.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import * as Helper from '../support/helpers';
+
+describe('Private Routes', () => {
+  it('Should logout if survey-list has no token', () => {
+    cy.visit('')
+    Helper.testUrl('/login')
+  });
+});
