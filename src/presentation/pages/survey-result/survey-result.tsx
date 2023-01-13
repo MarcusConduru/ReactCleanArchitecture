142d2b3ffbc9f27dbbb642b8b6c61cab173fbdc4
marcus.conduru <marcus.conduru@gmail.com>
2023-01-13 19:19:02 -0300

    feat: ensure SurveyResult calls LoadSurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -5,16 +5,24 @@ import {
   Calendar,
   Error,
 } from '@/presentation/components';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Styles from './survey-result-styles.scss';
 import { LoadSurveyResult } from '@/domain/usecases';
 
-const SurveyResult: React.FC = () => {
+type Props = {
+  loadSurveyResult: LoadSurveyResult;
+};
+
+const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
   const [state] = useState({
     isLoading: false,
     error: '',
     SurveyResult: null as LoadSurveyResult.Model,
   });
+
+  useEffect(() => {
+    loadSurveyResult.load().then().catch();
+  }, []);
 
   return (
     <div className={Styles.surveyResultWrap}>
