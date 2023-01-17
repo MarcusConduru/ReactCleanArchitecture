1e1c2de827d5b580bca86696d45b31f34bfcf3a5
marcus.conduru <marcus.conduru@gmail.com>
2023-01-17 18:46:38 -0300

    feat: ensure SurveyList redirect to SurveyResult

--- src/presentation/pages/survey-list/components/item/item.tsx
+++ src/presentation/pages/survey-list/components/item/item.tsx
@@ -2,13 +2,14 @@ import { LoadSurveyList } from '@/domain/usecases';
 import { Calendar, Icon, IconName } from '@/presentation/components';
 import React from 'react';
 import Styles from './item-styles.scss';
+import { Link } from 'react-router-dom';
 
 type Props = {
   survey: LoadSurveyList.Model;
 };
 
 const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
-  const iconName = survey.didAnswer ? IconName.thumbUp : IconName.thumbDown
+  const iconName = survey.didAnswer ? IconName.thumbUp : IconName.thumbDown;
   return (
     <li className={Styles.surveyItemWrap}>
       <div className={Styles.surveyContent}>
@@ -16,7 +17,11 @@ const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
         <Calendar date={survey.date} className={Styles.CalendarWrap} />
         <p data-testid="question">{survey.question}</p>
       </div>
-      <footer>Ver Resultado</footer>
+      <footer>
+        <Link data-testid="link" to={`/surveys/${survey.id}`}>
+          Ver Resultado
+        </Link>
+      </footer>
     </li>
   );
 };
